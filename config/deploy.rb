89d61e2676cba7f183554c7ecdd5ac224f2ac31b
GHANSHYAM ANAND <ghanshyamanand@fizzysoftware.com>
2016-09-14 00:02:16 +0530

    changes made

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,17 +1,23 @@
 # config valid only for current version of Capistrano
 lock '3.6.1'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'sonu_app'
+set :repo_url, 'https://github.com/ghanshyamanand/sonu_app.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
 # set :deploy_to, '/var/www/my_app_name'
+set (:deploy_to) { "/home/ghanshyam/#{application}/#{stage}" }
+
 
 # Default value for :scm is :git
-# set :scm, :git
+set :scm, :git
+
+set :rvm_ruby_string, '2.1.8'             # ruby version you are using...
+set :rvm_type, :user
+
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
@@ -24,13 +30,13 @@ set :repo_url, 'git@example.com:me/my_repo.git'
 # set :pty, true
 
 # Default value for :linked_files is []
-# append :linked_files, 'config/database.yml', 'config/secrets.yml'
+append :linked_files, 'config/database.yml', 'config/secrets.yml'
 
 # Default value for linked_dirs is []
-# append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
 # Default value for keep_releases is 5
-# set :keep_releases, 5
+set :keep_releases, 5
